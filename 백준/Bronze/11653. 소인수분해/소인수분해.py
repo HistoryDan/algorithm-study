48345e0c5cd95bfb23195994db5dc6b9ae368fa3
Junhyeong Park <82267460+HistoryDan@users.noreply.github.com>
2023-09-19 13:08:51 +0900

    [Bronze I] Title: 소인수분해, Time: 1872 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -8,8 +8,9 @@ for i in range(2, n+1):
 for factor in factors:
   if factor == 2 or factor % 2 != 0 :
     for i in range(2,factor):
-      if factor % 2 == 0:
-        factors.remove(i)
+      if factor % i == 0:
+        factors.remove(factor)
+        break
 
 for factor in factors:
   while n % factor == 0:
