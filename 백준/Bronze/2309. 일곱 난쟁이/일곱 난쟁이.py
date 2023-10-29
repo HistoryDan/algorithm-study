56c21f389aba307227a8d259fa8d82ce0abe905b
Junhyeong Park <82267460+HistoryDan@users.noreply.github.com>
2023-10-29 12:57:58 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -0,0 +1,16 @@
+import sys
+import itertools
+
+hobits = [0 for _ in range(9)]
+for i in range(9):
+    hobit = int(sys.stdin.readline().rstrip())
+    hobits[i] = hobit
+
+combs = itertools.combinations(hobits, 7)
+
+for comb in combs:
+    if sum(comb) == 100:
+        comb = sorted(comb)
+        for c in comb:
+            print(c)
+        break
