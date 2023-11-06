2ade1006c16ca7f957cad1ed5111575b0467c27f
Junhyeong Park <82267460+HistoryDan@users.noreply.github.com>
2023-11-06 12:29:40 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+    long long int a, b;
+    cin >> a >> b;
+    long long int d = abs(a - b);
+    cout << d;
+    return 0; 
+}
