4b6c46662651cb28d2035ed7387d55fef2c003b4
Junhyeong Park <82267460+HistoryDan@users.noreply.github.com>
2023-11-09 09:26:44 +0900

    [Bronze III] Title: 직각삼각형, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,12 @@
+import sys
+
+while True:
+    a,b,c = map(int, sys.stdin.readline().rstrip().split())
+    if (a==0) and (b==0) and (c==0):
+        break
+    sides = sorted([a,b,c])
+    if sides[2] ** 2 == sides[0] ** 2 + sides[1] ** 2:
+        print('right')
+    else:
+        print('wrong')
+    
