e630ea4bfe59f5a1b1b308f7f0697dbd06627814
Junhyeong Park <82267460+JunBro1016@users.noreply.github.com>
2023-12-29 20:30:53 +0900

    [Bronze IV] Title: 특별한 날, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.py
@@ -0,0 +1,11 @@
+import sys
+
+month = int(sys.stdin.readline().rstrip())
+day = int(sys.stdin.readline().rstrip())
+
+if month < 2 : print('Before')
+elif month > 2 : print('After')
+else:
+    if day < 18 : print('Before')
+    elif day > 18 : print('After')
+    else: print('Special')
