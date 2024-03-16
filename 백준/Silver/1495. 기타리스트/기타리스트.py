1abdcca6e0be9cbce98677019ff4b652b4b2d9e9
Junhyeong Park <82267460+junbro1016@users.noreply.github.com>
2024-03-16 18:17:06 +0900

    [Silver I] Title: 기타리스트, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1495. 기타리스트/기타리스트.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+N, S, M = map(int, input().split())
+V = list(map(int, input().split()))
+dp = [[0]*(M+1) for _ in range(N+1)]
+dp[0][S] = 1
+
+for i in range(1, N+1):
+    for j in range(M+1):
+        if dp[i-1][j] == 0:
+            continue
+        if j - V[i-1] >= 0:
+            dp[i][j - V[i-1]] = 1
+        if j + V[i-1] <= M:
+            dp[i][j + V[i-1]] = 1
+
+for i in range(M, -1, -1):
+    if dp[N][i] == 1:
+        print(i)
+        break
+else:
+    print(-1)
