29a717383878799bd8e5ae7a374f6856b9991625
Junhyeong Park <82267460+junbro1016@users.noreply.github.com>
2024-03-24 19:56:29 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -0,0 +1,23 @@
+import math
+from collections import deque
+
+def solution(progresses, speeds):
+    days = [math.ceil((100 - progresses[i]) / speeds[i]) for i in range(len(speeds))]
+    answer = []
+    
+    stack = deque()
+    stack.append(days[0])
+    largest = days[0]
+    
+    for i in range(1, len(days)):
+        cur = days[i]
+        if cur <= largest:
+            stack.append(cur)
+        else:
+            largest = cur
+            answer.append(len(stack))
+            stack.clear()
+            stack.append(largest)
+    
+    answer.append(len(stack))
+    return answer
