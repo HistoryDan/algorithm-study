04e5e267f3466e508b6f36e00a85e4adc10d4046
Junhyeong Park <82267460+junbro1016@users.noreply.github.com>
2024-05-07 11:34:26 +0900

    [Silver I] Title: 1, 2, 3 더하기 7, Time: 300 ms, Memory: 154640 KB -BaekjoonHub

---
+++ 백준/Silver/15992. 1， 2， 3 더하기 7/1， 2， 3 더하기 7.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+dp = [[1], [1,1], [1,2,1]]
+
+for i in range(3,1001):
+    new = [0] * (i+1)
+    first = [0] + dp[i-1]
+    second = [0] + dp[i-2] + [0]
+    third = [0] + dp[i-3] + [0,0]
+    for j in range(i+1):
+        cur = first[j] + second[j] + third[j]
+        new[j] = cur
+    dp.append(new)
+
+T = int(input())
+for _ in range(T): 
+    n,m = map(int, input().split())
+    print(dp[n-1][m-1] % 1000000009)
